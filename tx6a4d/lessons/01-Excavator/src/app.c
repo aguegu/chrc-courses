7791438caa500f3b2cb432d08f72e3e402a485b2
aGuegu <weihong.guan@gmail.com>
2025-11-25 18:24:33 +0800

    move instructions to markdown

--- tx6a4d/lessons/01-Excavator/src/app.c
+++ tx6a4d/lessons/01-Excavator/src/app.c
@@ -1,16 +1,5 @@
 #include "app.h"
 
-// 遥控器：
-// 按键： Buttons[3], Buttons[2], Buttons[1], Buttons[0] (从左到右)
-// 左摇杆：                右摇杆：
-//    上下： Sticks[2]        上下： Sticks[1]
-//    左右： Sticks[3]        左右： Sticks[0]
-// 左旋钮： Sticks[4]      右旋钮： Sticks[5]
-
-// 接收机：
-// 舵机： Channels[4], Channels[5], Channels[6], Channels[7] (从左到右)
-// 电机： Channels[0], Channels[1], Channels[2], Channels[3] (从左到右)
-
 void app() {
   setChannel(0, getStick(2));
   setChannel(1, getStick(3));
