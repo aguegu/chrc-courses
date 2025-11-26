97fb33180970cc9c86fcf0cb4ad0cdc39ee9997f
aGuegu <weihong.guan@gmail.com>
2025-11-26 15:23:00 +0800

    init rx4m4s course

---
+++ rx4m4s/lessons/00-Default/inc/app.h
@@ -0,0 +1,13 @@
+#ifndef APP_H
+#define APP_H
+
+#include <stdint.h>
+
+int8_t getChannel(uint8_t index);
+
+void setMotor(uint8_t index, int8_t value);
+void setServo(uint8_t index, int8_t value);
+
+void app();
+
+#endif
