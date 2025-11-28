4b052c6e59f77dd30b4d47107776dd88f7b2f75f
aGuegu <weihong.guan@gmail.com>
2025-11-28 10:43:59 +0800

    fix typ in rx default lesson

--- rx4m4s/lessons/00-Default/inc/app.h
+++ rx4m4s/lessons/00-Default/inc/app.h
@@ -6,7 +6,7 @@
 int8_t getChannel(uint8_t index);
 
 void setMotor(uint8_t index, int8_t value);
-void setServo(uint8_t index, int8_t value);
+void setServo(uint8_t index, uint8_t value);
 
 void loop();
 
