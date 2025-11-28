1f184f842f93780c493e630290aa63261f6acd71
aGuegu <weihong.guan@gmail.com>
2025-11-28 17:28:28 +0800

    fix turn direction

--- tx6a4d/lessons/04-DumpTruck/src/app.c
+++ tx6a4d/lessons/04-DumpTruck/src/app.c
@@ -10,11 +10,10 @@ int8_t getStickNeutral(uint8_t index, uint8_t deadzone) {
 
 void loop() {
   setChannel(1, - getStickNeutral(2, 5) / 2); // Engine
-  setChannel(2, - getStickNeutral(1, 40)); // Lift
+  setChannel(2, - getStickNeutral(1, 0x40)); // Lift
 
   setChannel(3, - getStick(4)); // Led
 
-  setChannel(4, - getStickNeutral(0, 5) / 2); // Turn
+  setChannel(4, getStickNeutral(0, 5) / 2); // Turn
   setChannel(5, getStick(5)); // differential Lock
-
 }
