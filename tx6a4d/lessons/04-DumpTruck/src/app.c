ef32bb136d0d04aa7a9d9ed44225ec2c0bdc21eb
aGuegu <weihong.guan@gmail.com>
2025-11-28 11:24:31 +0800

    dump truck demo

---
+++ tx6a4d/lessons/04-DumpTruck/src/app.c
@@ -0,0 +1,20 @@
+#include "app.h"
+
+#ifndef ABS
+#define ABS(n)     (((n) < 0) ? -(n) : (n))
+#endif
+
+int8_t getStickNeutral(uint8_t index, uint8_t deadzone) {
+  return ABS(getStick(index)) > deadzone ? getStick(index) : 0;
+}
+
+void loop() {
+  setChannel(1, - getStickNeutral(2, 5) / 2); // Engine
+  setChannel(2, - getStickNeutral(1, 40)); // Lift
+
+  setChannel(3, - getStick(4)); // Led
+
+  setChannel(4, - getStickNeutral(0, 5) / 2); // Turn
+  setChannel(5, getStick(5)); // differential Lock
+
+}
