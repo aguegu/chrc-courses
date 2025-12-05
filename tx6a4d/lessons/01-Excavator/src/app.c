92c160e381be2e58f7f1358b8ce51343ed497255
aGuegu <weihong.guan@gmail.com>
2025-12-05 15:26:26 +0800

    lower motor freq to 5hz

--- tx6a4d/lessons/01-Excavator/src/app.c
+++ tx6a4d/lessons/01-Excavator/src/app.c
@@ -1,27 +1,43 @@
 #include "app.h"
 
+#ifndef ABS
+#define ABS(n)     (((n) < 0) ? -(n) : (n))
+#endif
+
+#ifndef MIN
+#define MIN(n,m)   (((n) < (m)) ? (n) : (m))
+#endif
+
+#ifndef MAX
+#define MAX(n,m)   (((n) < (m)) ? (m) : (n))
+#endif
+
+int8_t getStickNeutral(uint8_t index, uint8_t deadzone) {
+  return ABS(getStick(index)) > deadzone ? getStick(index) : 0;
+}
+
 void loop() {
-  setChannel(0, getStick(2));
-  setChannel(1, getStick(3));
-  setChannel(2, getStick(0));
-  setChannel(3, getStick(1));
+  setChannel(0, getStickNeutral(0, 0x20));    // Bucket
+  setChannel(1, - getStickNeutral(1, 0x20));  // Boom
+  setChannel(2, - getStickNeutral(2, 0x20));  // Dipper
+  setChannel(3, getStickNeutral(3, 0x20) / 2);    // Swing
 
-  setChannel(4, getStick(4));
-  setChannel(5, getStick(5));
+  setChannel(6, MIN(getStick(4), 0)); // Blade
+  setChannel(7, getStick(5)); // Crawler
 
-  if (getButton(0) == getButton(1)) {
-    setChannel(6, 0);
+  if (getButton(0) == getButton(1)) { // Left Motor
+    setChannel(5, 0);
   } else if (getButton(1)) {
-    setChannel(6, 127);
+    setChannel(5, 0x60);
   } else {  // getButton(0)
-    setChannel(6, -127);
+    setChannel(5, -0x60);
   }
 
-  if (getButton(2) == getButton(3)) {
-    setChannel(7, 0);
+  if (getButton(2) == getButton(3)) { // Right Motor
+    setChannel(4, 0);
   } else if (getButton(2)) {
-    setChannel(7, 127);
+    setChannel(4, 0x60);
   } else {  // getButton(3)
-    setChannel(7, -127);
+    setChannel(4, -0x60);
   }
 }
