3494e08e4cbea89e25fa7f4705d95c31a45a55aa
aGuegu <weihong.guan@gmail.com>
2025-12-11 13:48:25 +0800

    excavator with x control

---
+++ tx6a4d/lessons/04-Excavator-X/README.md
@@ -0,0 +1,34 @@
+遥控器
+===
+
+按键:
+
+| 左 | | | 右 |
+| - | - | - | - |
+| Buttons[0]  | Buttons[1] | Buttons[2] | Buttons[3] |
+
+摇杆:
+
+|  | 左 | 右 |
+| - | - | - |
+| 上下 | Sticks[2] | Sticks[1] |
+| 左右 | Sticks[3] | Sticks[0] |
+
+旋钮：
+| 左 | 右 |
+| - | - |
+| Sticks[4] | Sticks[5] |
+
+接收机
+===
+
+舵机：
+
+| 左 | | | 右 |
+| - | - | - | - |
+| Channels[4]  | Channels[5] | Channels[6] | Channels[7] |
+
+电机： 
+| 左 | | | 右 |
+| - | - | - | - |
+| Channels[0] | Channels[1] | Channels[2] | Channels[3] |
---
+++ tx6a4d/lessons/04-Excavator-X/src/app.c
@@ -0,0 +1,54 @@
+#include "app.h"
+
+#ifndef ABS
+#define ABS(n)     (((n) < 0) ? -(n) : (n))
+#endif
+
+#ifndef MIN
+#define MIN(n,m)   (((n) < (m)) ? (n) : (m))
+#endif
+
+#ifndef MAX
+#define MAX(n,m)   (((n) < (m)) ? (m) : (n))
+#endif
+
+int16_t maxIn3(int16_t a, int16_t b, int16_t c) {
+  int16_t m = MAX(a, b);
+  return MAX(m, c);
+}
+
+int8_t getStickNeutral(uint8_t index, uint8_t deadzone) {
+  return ABS(getStick(index)) > deadzone ? getStick(index) : 0;
+}
+
+void loop() {
+  int16_t left = getStickNeutral(1, 0x10) + getStickNeutral(0, 0x10);
+  int16_t right = getStickNeutral(1, 0x10) - getStickNeutral(0, 0x10);
+
+  int16_t m = maxIn3(ABS(left), ABS(right), 127);
+
+  setChannel(4, left * 127. / m); // Left Motor
+  setChannel(5, right * 127. / m);  // Right Motor
+
+  setChannel(2, - getStickNeutral(2, 0x20));  // Dipper
+  setChannel(3, getStickNeutral(3, 0x20) / 2);    // Swing
+
+  setChannel(6, MIN(getStick(4), 0)); // Blade
+  setChannel(7, getStick(5)); // Crawler
+
+  if (getButton(0) == getButton(1)) { // Bucket
+    setChannel(0, 0);
+  } else if (getButton(1)) {
+    setChannel(0, 0x60);
+  } else {  // getButton(0)
+    setChannel(0, -0x60);
+  }
+
+  if (getButton(2) == getButton(3)) { // Boom
+    setChannel(1, 0);
+  } else if (getButton(2)) {
+    setChannel(1, 0x60);
+  } else {  // getButton(3)
+    setChannel(1, -0x60);
+  }
+}
