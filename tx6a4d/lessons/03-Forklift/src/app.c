e30eed419bda39bea4a40b18b3b8d26d4e7673f0
aGuegu <weihong.guan@gmail.com>
2025-12-30 17:23:22 +0800

    adopt square(x) in motor control

--- tx6a4d/lessons/03-Forklift/src/app.c
+++ tx6a4d/lessons/03-Forklift/src/app.c
@@ -1,20 +1,19 @@
 #include "app.h"
 
-#ifndef ABS
-#define ABS(n)     (((n) < 0) ? -(n) : (n))
-#endif
-
-int8_t getStickNeutral(uint8_t index, uint8_t deadzone) {
-  return ABS(getStick(index)) > deadzone ? getStick(index) : 0;
+int8_t stickToMotor(uint8_t index, uint8_t deadzone) {
+  if (getStick(index) < deadzone && getStick(index) > -deadzone) {
+    return 0;
+  }
+  double f = getStick(index) / 127.0;
+  return (int8_t)(f * f * 127.0 * (f > 0 ? 1 : -1));
 }
 
 void loop() {
-  setChannel(0, getStickNeutral(3, 5) / 2); // Pitch
-  setChannel(1, - getStickNeutral(2, 5) / 2); // Engine
-  setChannel(2, getStickNeutral(1, 5)); // Lift
+  setChannel(0, stickToMotor(3, 5) / 2); // Pitch
+  setChannel(1, - stickToMotor(2, 5) / 2); // Engine
+  setChannel(2, stickToMotor(1, 5)); // Lift
 
   setChannel(3, - getStick(4)); // Led
 
-  setChannel(4, - getStickNeutral(0, 5) / 2); // Turn
-
+  setChannel(4, getStick(0) / 2); // Turn
 }
