e05d63561589bf7b61ce0c385cfbbffa56df2c99
aGuegu <weihong.guan@gmail.com>
2026-02-08 16:40:04 +0800

    forklift combo

---
+++ tx6a4d/lessons/06-Combo/src/app.c
@@ -0,0 +1,55 @@
+#include "app.h"
+
+#ifndef ABS
+#define ABS(n)     (((n) < 0) ? -(n) : (n))
+#endif
+
+uint8_t gear = 3;
+
+int8_t getStickNeutral(uint8_t index, uint8_t deadzone) {
+  return ABS(getStick(index)) > deadzone ? getStick(index) : 0;
+}
+
+void loop() {
+
+  static bool buttonsLast[4] = { false, false, false, false };
+
+
+  setChannel(4, getStickNeutral(0, 8) * 3 / 4 + getStick(5) / 4); // SM0
+
+  setChannel(0, getStickNeutral(2, 3) * gear / 3); // DM0
+
+  setChannel(1, getStickNeutral(3, 6)); // DM1
+
+  setChannel(2, getStickNeutral(1, 3)); // DM2
+
+  setChannel(5, getStick(4)); // SM1
+
+  if (getButton(0) && !buttonsLast[0]) {
+    if (gear > 1) gear--;
+  }
+
+  if (getButton(1) && !buttonsLast[1]) {
+    if (gear < 3) gear++;
+  }
+
+  if (getButton(2) && !buttonsLast[2]) {
+    if (getChannel(3)) {
+      setChannel(3, 0);
+    } else {
+      setChannel(3, 40);
+    }
+  }
+
+  if (getButton(3) && !buttonsLast[3]) {
+    if (getChannel(3)) {
+      setChannel(3, 0);
+    } else {
+      setChannel(3, -40);
+    }
+  }
+
+  for (uint8_t i = 0; i < 4; i++) {
+    buttonsLast[i] = getButton(i);
+  }
+}
