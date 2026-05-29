134ba4ff3a51e608f4c917e1ed654cc0ff2c37db
aGuegu <weihong.guan@gmail.com>
2026-05-29 20:13:38 +0800

    update miniz demo with a single motor + single servo

--- tx6a4d/lessons/06-Combo/src/app.c
+++ tx6a4d/lessons/06-Combo/src/app.c
@@ -1,57 +1,30 @@
 #include "app.h"
 
-uint8_t gear = 3;
+#define STK_SPEED 4
+#define STK_TURN_MAIN 0
+#define STK_TURN_OFFSET 5
+#define STK_THROTTLE 1
 
-int8_t getStickNeutral(uint8_t index, uint8_t deadzone) {
-  int8_t v = getStick(index);
+int8_t getStickNeutral(uint8_t index, int8_t offset, uint8_t deadzone) {
+  int16_t v = getStick(index) + offset;
+  if (v > 127) v = 127;
+  if (v < -127) v = -127;
+
   if (v > deadzone) {
-    return (float)(v - deadzone) / (127 - deadzone) * 127;
+    return (int8_t)((v - deadzone) * 127 / (127 - deadzone));
   } else if (v < -deadzone) {
-    return (float)(v + deadzone) / (127 - deadzone) * 127;
+    return (int8_t)((v + deadzone) * 127 / (127 - deadzone));
   }
   return 0;
 }
 
 void loop() {
+  uint16_t speed = (getStick(STK_SPEED) + 127);
+  setChannel(4, - getStick(STK_TURN_MAIN) * 3 / 4 + getStick(STK_TURN_OFFSET) / 4);
 
-  static bool buttonsLast[4] = { false, false, false, false };
-
-
-  setChannel(4, getStickNeutral(0, 8) * 3 / 4 + getStick(5) / 4); // SM0
-
-  setChannel(0, getStickNeutral(2, 3) * gear / 3); // DM0
-
-  setChannel(1, getStickNeutral(3, 6)); // DM1
-
-  setChannel(2, getStickNeutral(1, 3)); // DM2
-
-  setChannel(5, getStick(4)); // SM1
-
-  if (getButton(0) && !buttonsLast[0]) {
-    if (gear > 1) gear--;
-  }
-
-  if (getButton(1) && !buttonsLast[1]) {
-    if (gear < 3) gear++;
-  }
-
-  if (getButton(2) && !buttonsLast[2]) {
-    if (getChannel(3)) {
-      setChannel(3, 0);
-    } else {
-      setChannel(3, 40);
-    }
-  }
-
-  if (getButton(3) && !buttonsLast[3]) {
-    if (getChannel(3)) {
-      setChannel(3, 0);
-    } else {
-      setChannel(3, -40);
-    }
-  }
-
-  for (uint8_t i = 0; i < 4; i++) {
-    buttonsLast[i] = getButton(i);
+  if (getButton(0)) {
+    setChannel(0, -128);
+  } else {
+    setChannel(0, getStickNeutral(STK_THROTTLE, 0, 2) * speed / 254);
   }
 }
